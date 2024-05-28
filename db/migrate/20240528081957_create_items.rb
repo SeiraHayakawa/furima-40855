e4191effee4015ffc03f270e33ac39c5ca70ee20
SeiraHayakawa <peterseira0824@gmail.com>
2024-05-28 17:47:21 +0900

    モデル、マイグレーションファイル設定

--- db/migrate/20240528081957_create_items.rb
+++ db/migrate/20240528081957_create_items.rb
@@ -1,6 +1,15 @@
 class CreateItems < ActiveRecord::Migration[7.0]
   def change
     create_table :items do |t|
+      t.string :item_name, null: false 
+      t.text :introduction, null: false
+      t.integer :category_id, null: false
+      t.integer :situation_id, null: false
+      t.integer :fee_id, null: false
+      t.integer :prefecture_id, null: false
+      t.integer :institution_id, null: false
+      t.integer :price, null: false
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -40,8 +40,18 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_28_082831) do
   end
 
   create_table "items", charset: "utf8", force: :cascade do |t|
+    t.string "item_name", null: false
+    t.text "introduction", null: false
+    t.integer "category_id", null: false
+    t.integer "situation_id", null: false
+    t.integer "fee_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "institution_id", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
@@ -64,4 +74,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_28_082831) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "items", "users"
 end
