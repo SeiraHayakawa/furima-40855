d188fb675bee62a86f3f14dd10e141879e1ea420
SeiraHayakawa <peterseira0824@gmail.com>
2024-05-28 18:49:35 +0900

    コントローラーとactivhashモデルのバリテーション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,15 +4,22 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :introduction, :category_id, :situation_id, :fee_id, :prefecture_id, :institution_id, :price, :image).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,35 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one :buyer
+
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :fee
+  belongs_to :institution
+  belongs_to :situation
+  belongs_to :Prefecture
 
+  
+  validates :item_name, :institution, :price, presence: true
+
+
+  validates :category_id, :situation_id, :fee_id, :prefecture_id, :institution_id, numericality: { other_than: 1, message: "can't be blank" } 
+
+end
+
+
+create_table :items do |t|
+  t.string :item_name, null: false 
+  t.text :introduction, null: false
+  t.integer :category_id, null: false
+  t.integer :situation_id, null: false
+  t.integer :fee_id, null: false
+  t.integer :prefecture_id, null: false
+  t.integer :institution_id, null: false
+  t.integer :price, null: false
+  t.references :user, null: false, foreign_key: true
+
+  t.timestamps
 end
