5a1d41d993b3e0f7fb56416f370cb73252296aca
SeiraHayakawa <peterseira0824@gmail.com>
2024-05-29 12:42:14 +0900

    モデルのバリテーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one :buyer
-
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
@@ -9,10 +8,15 @@ class Item < ApplicationRecord
   belongs_to :fee
   belongs_to :institution
   belongs_to :situation
-  belongs_to :Prefecture
+  belongs_to :prefecture
 
-  
-  validates :item_name, :introduction, :image, :price, presence: true
+
+  validates :item_name, presence: true, length: { maximum: 40 }
+  validates :introduction, presence: true, length: { maximum: 1000 }
+  validates :image, presence: true
+  validates :price, presence: true
+  validates :price, numericality: { only_integer: true, message: "must be a half-width number" }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "must be between ¥300 and ¥9,999,999" }
 
 
   validates :category_id, :situation_id, :fee_id, :prefecture_id, :institution_id, numericality: { other_than: 1, message: "can't be blank" } 
