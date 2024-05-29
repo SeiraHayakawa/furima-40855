c30d7f200fcc8fe5bb5c3e9687d7f8124dff7e82
SeiraHayakawa <peterseira0824@gmail.com>
2024-05-29 11:12:31 +0900

    ビューの条件定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,7 @@ class Item < ApplicationRecord
   belongs_to :Prefecture
 
   
-  validates :item_name, :institution, :price, presence: true
+  validates :item_name, :introduction, :image, :price, presence: true
 
 
   validates :category_id, :situation_id, :fee_id, :prefecture_id, :institution_id, numericality: { other_than: 1, message: "can't be blank" } 
