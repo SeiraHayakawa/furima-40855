f0ee9e448d86f14183331169846ed343a0192d64
SeiraHayakawa <peterseira0824@gmail.com>
2024-05-30 17:03:03 +0900

    javascriptの実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,17 @@
+
+const priceInput = document.getElementById("item-price");
+const taxPriceDom = document.getElementById("add-tax-price");
+const profitDom = document.getElementById("profit");
+
+priceInput.addEventListener("input", () => {
+  const inputValue = priceInput.value;
+  const tax = Math.floor(inputValue * 0.1);
+  const profit = inputValue - tax;
+
+  const addTaxDom = document.getElementById("add-tax-price");
+  addTaxDom.innerHTML = tax
+
+  const addProfitDom = document.getElementById("profit");
+  addProfitDom.innerHTML = profit
+
+})
